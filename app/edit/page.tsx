adc347f2bfee20edf17926a15a8b68b4b95ddcd2
Ippei523 <ippei523@gmail.com>
2024-01-20 17:15:18 +0900

    編集ページのみ作成

---
+++ app/edit/page.tsx
@@ -0,0 +1,18 @@
+export default function Page() {
+  return (
+    <div className="todo-container">
+      <h1>Todo App</h1>
+      <div className="todo">
+        <div>
+          <p>タイトル</p>
+          <input/>
+        </div>
+        <div>
+          <p>内容</p>
+          <textarea/>
+        </div>
+        <button>追加</button>
+      </div>
+    </div>
+  );
+}
