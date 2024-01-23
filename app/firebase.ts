44f20ed0342df32bedf1d88d7a8b8054086fc823
Ippei523 <ippei523@gmail.com>
2024-01-24 00:36:31 +0900

    環境変数の設定

--- app/firebase.ts
+++ app/firebase.ts
@@ -7,15 +7,18 @@ import { getFirestore } from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDKn7Ctb2LcZiXsYt_ftxtM6xTDSw2IaAA",
-  authDomain: "study-create-useswr.firebaseapp.com",
-  databaseURL: "https://study-create-useswr-default-rtdb.firebaseio.com",
-  projectId: "study-create-useswr",
-  storageBucket: "study-create-useswr.appspot.com",
-  messagingSenderId: "770988997639",
-  appId: "1:770988997639:web:3b2fa68e7efca739befea2",
-  measurementId: "G-6B6R6ENNQW"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 }
+
+console.log(firebaseConfig)
+console.log(process.env.FIREBASE_API_KEY)
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
